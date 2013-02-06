5c73e96579c9b60cd941a94c1245ea86142c8ab6
Finlay McCourt <finlaymccourt@gmail.com>
2013-02-06 11:53:26 +0000

    Adding models for artist and location adding admin

---
+++ MapMyPlaylist/findartist/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from findartist.models import Artist, Location
+
+admin.site.register(Artist)
+admin.site.register(Location)
--- MapMyPlaylist/findartist/models.py
+++ MapMyPlaylist/findartist/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    bio = models.TextField(max_length=200)
+    image  = models.CharField(max_length=200)
+    origin = models.ForeignKey('Location')
 
+class Location(models.Model):
+    placename = models.CharField(max_length=200)
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
