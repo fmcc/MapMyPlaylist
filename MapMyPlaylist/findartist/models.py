edfdb469e1abdb6e416c252b553c115df49cd70f
Finlay McCourt <finlaymccourt@gmail.com>
2013-02-09 14:56:52 +0000

    Unfinished addartist methods

--- MapMyPlaylist/findartist/models.py
+++ MapMyPlaylist/findartist/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+import re
+import textwrap
 
 class Artist(models.Model):
     name = models.CharField(max_length=200)
     bio = models.TextField(max_length=200)
+
+    def formatted_bio(self):
+        nohtml = re.sub("<.*?>", "", self.bio)
+        return textwrap.fill(nohtml, 80)
+    
     image  = models.CharField(max_length=200)
-    origin = models.ForeignKey('Location')
+    origin = models.ForeignKey('Location', related_name='artist', to_field='placename')
 
 class Location(models.Model):
-    placename = models.CharField(max_length=200)
+    placename = models.CharField(max_length=200, unique=True)
     latitude = models.CharField(max_length=200)
     longitude = models.CharField(max_length=200)
