26de14694dc7f0d20e5411fc85e9e9c6b5bf445e
Finlay McCourt <finlaymccourt@gmail.com>
2013-02-25 15:28:15 +0000

    Converted views.py to return json objects.

--- MapMyPlaylist/findartist/views.py
+++ MapMyPlaylist/findartist/views.py
@@ -2,18 +2,26 @@ from findartist.models import Artist, Location
 from findartist.utils.CreateArtist import CreateArtist
 from findartist.utils.LastFMPlaylist import LastFMPlaylist
 from django.shortcuts import render_to_response, get_object_or_404
+from django.http import  HttpResponse 
+from django.utils.simplejson import dumps, loads, JSONEncoder
+from django.core import serializers
 
 def artistQuery(request, artistName):
-    artist = artistGetOrCreate(artistName)
-    return render_to_response('findartist.html',{'artist': artist})
+    a = artistGetOrCreate(artistName)
+    artist = []
+    artist.append({'name': a.name,'bio': a.bio,'img_url': a.image,'origin': a.origin.placename,'lat': a.origin.latitude,'long': a.origin.longitude})
+    json_artist = dumps(artist)
+    return HttpResponse(json_artist, content_type="application/json")
 
 def playlistQuery(request, lastFMUsername):
     user = LastFMPlaylist(lastFMUsername)
     artists = user.getPlaylist()
     playlist = []
     for art in artists:
-        playlist.append(artistGetOrCreate(art))
-    return render_to_response('playlist.html',{'playlist': playlist})
+        a = artistGetOrCreate(art)
+        playlist.append({'name': a.name,'bio': a.bio,'img_url': a.image,'origin': a.origin.placename,'lat': a.origin.latitude,'long': a.origin.longitude})
+    json_playlist = dumps(playlist)
+    return HttpResponse(json_playlist, content_type="application/json") 
 
 def artistGetOrCreate(artistName):
     try:
