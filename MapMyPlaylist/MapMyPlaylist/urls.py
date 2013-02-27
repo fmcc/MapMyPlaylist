d72cdba52ddf3acd1dd37f7594887ba34f1bcd12
Finlay McCourt <finlaymccourt@gmail.com>
2013-02-27 21:40:51 +0000

    Began user login. Quick working test of timed ajax.

--- MapMyPlaylist/MapMyPlaylist/urls.py
+++ MapMyPlaylist/MapMyPlaylist/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^$', 'mapfrontend.views.mainpage'),
+    url(r'^register/$', 'mapfrontend.views.register'),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
--- MapMyPlaylist/mapfrontend/views.py
+++ MapMyPlaylist/mapfrontend/views.py
@@ -1,5 +1,29 @@
-# Create your views here.
-from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.models import User
+from mapfrontend.models import UserProfile, UserForm, UserProfileForm
+from django.shortcuts import render_to_response
 
 def mainpage(request):
-   return render_to_response('mainpage.html') 
+    return render_to_response('mainpage.html')
+
+def register(request):
+    context = RequestContext(request)
+    registered = False
+    if request.method == 'POST':
+        uform = UserForm(data = request.POST)
+        pform = UserProfileForm(data = request.POST)
+        if uform.is_valid() and pform.is_valid():
+            user = uform.save()
+            pw = user.password
+            user.set_password(pw)
+            user.save()
+            profile = pform.save(commit = False)
+            profile.user = user
+            profile.save()
+            registered = True
+        else:
+            print uform.errors, pform.errors
+    else:
+        uform = UserForm()
+        apform = UserProfileForm()
+
+    return render_to_response('rango/register.html', {'uform': uform, 'pform': pform, 'registered': registered }, context)
