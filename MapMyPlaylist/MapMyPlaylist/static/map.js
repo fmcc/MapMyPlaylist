68c53ee632f45c7deea597ba31602b99fe17f611
Graeme Erskine <graeme@graemearthur.com>
2013-02-28 13:02:45 +0000

    Mapping geodata! (sort of) Managed to map first response but fails when no geodata found.

--- MapMyPlaylist/MapMyPlaylist/static/map.js
+++ MapMyPlaylist/MapMyPlaylist/static/map.js
@@ -29,6 +29,45 @@
 	    });
     	}   
     */
+$( document ).ready(function() 
+{
+  $('#startButton').click(function()
+  {
+    var username = $('input').val();
+    var latestArtist = "";
+
+    function getPlaylist()
+    {
+              // BUG: returns current tune at first but then removes
+     $.getJSON('/finduserplaylist/' + username + '/', function(data) 
+      {
+        var artists = "";
+                // checks to see if most recent artist has changed
+        if (data[0].name != latestArtist)
+        {
+          $.each(data, function() 
+          { // displays artist names currently for display purposes
+            var location = new L.LatLng(this.lat, this.long);
+            var name = this.name;
+            var image = this.image;
+            var marker = new L.Marker(location, { title: name });
+            map.addLayer(marker);
+          });
+          $('#artists').html(artists);
+          latestArtist = data[0].name;
+        }
+        })
+      // just for debug, remove if necessary
+      .success(function() {console.log("Playlist Updated!")})
+      .error(function() 
+      { 
+        console.log("Error, stopping refresh!");
+        clearInterval(playlistRefresh);
+      })
+    }
+    var playlistRefresh = setInterval(getPlaylist, 5000);
+  });
+});
     
     var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.');
     //    denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
