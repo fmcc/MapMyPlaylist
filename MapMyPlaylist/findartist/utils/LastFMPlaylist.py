0498cee9c0eaf40d234b6ac58db89a7cd4634fc5
Finlay McCourt <finlaymccourt@gmail.com>
2013-03-10 14:13:54 +0000

    The Minutemen finally have a location.

--- MapMyPlaylist/findartist/utils/LastFMPlaylist.py
+++ MapMyPlaylist/findartist/utils/LastFMPlaylist.py
@@ -1,14 +1,11 @@
 import pylast
 from findartist.utils.CreateArtist import CreateArtist
+from findartist.utils.generic import MMPLFMACC
 
 class LastFMPlaylist:
-    API_KEY = 'b9403e2443856fa0ddbc7dd991c5f6c8'
-    API_SECRET = '3030c906e7dfc4e12be5242fd0711604'
-    username = 'MapMyPlaylist'
-    password_hash = pylast.md5('playlistmymap')
 
     def __init__(self, lastFMUsername):
-        network = pylast.LastFMNetwork(api_key = self.API_KEY, api_secret = self.API_SECRET, username = self.username, password_hash = self.password_hash)
+        network = MMPLFMACC()
         self.LastFMUser = network.get_user(lastFMUsername)
 
     def getPlaylist(self):
