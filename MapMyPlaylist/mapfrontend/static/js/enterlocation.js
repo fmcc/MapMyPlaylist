205e88a84ab834f9846f4f6b63e6b20220f6f4d0
Graeme Erskine <graeme@graemearthur.com>
2013-03-13 13:29:14 +0000

    Enterlocation now shows div giving user option to add origin Call addUnknownOrigin(artist{}) when needed

--- MapMyPlaylist/mapfrontend/static/js/enterlocation.js
+++ MapMyPlaylist/mapfrontend/static/js/enterlocation.js
@@ -15,10 +15,42 @@ function enterLocation(artist) {
 	});
 };
 
+$( document ).ready(function(){
+	$("#MMP_unknown_artist_prompt").hide();
+});
+
+
+function addUnknownOrigin(artist) {
+	var name = artist.name;
+	$("#MMP_unknown_artist_prompt").toggle().html('');
+	var textBox = $('<input>')
+	.prop('type', 'text').val($(name).text())
+	.prop('id', 'originbox');
+
+	var button = $('<input>')
+	.prop('type', 'submit').val($(name).text())
+	.prop('id', 'originbutton')
+	.width(40);
+
+	$("#MMP_unknown_artist_prompt").append(name).append(textBox).append(button);
+	setArtist(name);
+}
+
 function addOrigin(artist, location) {
-	$.post('/addorigin/' + location + '/', artist
-	)
-};
+	$.post('/addorigin/' + location + '/', artist, function() {
+		$("#MMP_unknown_artist_prompt").html('').hide();
+		console.log(artist + " is from " + location);
+	});
+}
+
+function setArtist(name){
+    $("#originbutton").click(function() {
+    	var loc = $('#originbox').val();
+		$.getJSON('/findartist/' + name + '/', function(data){
+		addOrigin(data[0], loc)
+		});			
+	});
+}
 
 $(function() {
     $("#locationbox").autocomplete( {
